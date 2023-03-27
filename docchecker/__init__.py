e8281a392b00051a7998b3a38e24e3a22a148db2
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-27 03:05:33 -0700

    asdf

--- docchecker/__init__.py
+++ docchecker/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+import openai
+import keys
+
+openai.organization = keys.organizationid
+openai.api_key = keys.apikey
+openai.Model.list()
 
 @app.route('/')
 def home():
@@ -7,10 +13,17 @@ def home():
 
 @app.route('/eprompt', methods=['POST', 'GET'])
 def eprompt():
+    output = ""
     if request.method == "POST":
         eprompt = request.form.get("eprompt")
-        return "Your prompt is " + eprompt
-    return render_template('index.html')
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+         messages=[
+        {"role": "system", "content": eprompt}
+        ]
+        )
+        output = completion.choices[0].message.content
+    return render_template('index.html', output=output)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- docchecker/gpt.py
+++ docchecker/gpt.py
@@ -9,7 +9,7 @@ openai.Model.list()
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "system", "content": "Write a paragraph about why snails are amazing."}
+        {"role": "system", "content": eprompt}
     ]
 )
 
