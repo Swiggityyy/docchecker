887592754129788581e0b19acb577b5186a1e3b6
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-27 01:13:02 -0700

    asfd

--- docchecker/gpt.py
+++ docchecker/gpt.py
@@ -1,7 +1,6 @@
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
 import keys
-import os
 
 openai.organization = keys.organizationid
 openai.api_key = keys.apikey
@@ -10,7 +9,7 @@ openai.Model.list()
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "system", "content": "Write a 3 paragraph essay about WW2's impact on the US economoy?"}
+        {"role": "system", "content": "Write a paragraph about why snails are amazing."}
     ]
 )
 
