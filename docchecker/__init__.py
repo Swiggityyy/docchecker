92a81a9f5d43737fb069a638031cfe9d11e7301e
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-27 02:26:47 -0700

    asdf

--- docchecker/__init__.py
+++ docchecker/__init__.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    return 'You entered: {}'.format(request.index['text'])
+@app.route('/eprompt', methods=['POST', 'GET'])
+def eprompt():
+    if request.method == "POST":
+        eprompt = request.form.get("eprompt")
+        return "Your prompt is " + eprompt
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
